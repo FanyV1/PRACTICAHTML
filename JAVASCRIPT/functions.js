2d363e88c27b032143be9b54e3c446423bd16ce7
FanyV1 <delgadopalaciosestefania@gmail.com>
2024-02-12 11:40:48 -0600

    cree una formulario y lo de la clase pasada

--- JAVASCRIPT/functions.js
+++ JAVASCRIPT/functions.js
@@ -1,3 +1,9 @@
 function changeColor(){
   document.getElementById("palabrita").style.backgroundColor="green";
+  document.getElementById("palabrita").texrContent="hola Fany";
+
+}
+
+function enviarMsg(){
+  alert("hola amigo");
 }
